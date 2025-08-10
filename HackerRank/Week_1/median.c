97e878fcfc41e88dfb7e62e7babb475a7314ece1
Abhi-lash27 <babhilash27@gmail.com>
2025-08-10 09:11:02 +0530

    Added HackerRank Week_1 (Mock Test Soln) - Median

---
+++ HackerRank/Week_1/median.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int compare(const void *a,const void *b){
+    return (*(int*)a - *(int*)b);
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[1000001];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    qsort(arr,n,sizeof(int),compare);
+    
+    int median = n/2;
+    printf("%d",arr[median]);
+}
